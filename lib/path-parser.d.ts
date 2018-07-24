09a9f310408e63122d8ed715617b0da3b2a62ec5
ChenYong <chenyong@jimeng.io>
2018-07-24 20:01:57 +0800

    use generated d.ts; bump 0.2.3

---
+++ lib/path-parser.d.ts
@@ -0,0 +1,16 @@
+export interface IRouteRule {
+    path: string;
+    name?: string;
+    router?: IRouteRule[];
+}
+export interface IRouteParseResult {
+    matches: boolean;
+    name: string;
+    data: any;
+    restPath: string[];
+    basePath: string[];
+    next?: IRouteParseResult;
+    rules?: IRouteRule[];
+    params?: any;
+}
+export declare let parseRoutePath: (pathString: string, rules: IRouteRule[]) => IRouteParseResult;

9779a91910d12bb858c863e72fb1ff8be2db9e89
ChenYong <chenyong@jimeng.io>
2018-11-06 18:21:24 +0800

    add basic test cases

---
+++ src/path-parser.test.ts
@@ -0,0 +1,62 @@
+import { parseRoutePath, IRouteRule, IRouteParseResult } from "./path-parser";
+
+interface ISimplifiedResult {
+  name: string;
+  params: { [k: string]: string };
+  next?: ISimplifiedResult;
+}
+
+let simplifyResult = (r: IRouteParseResult): ISimplifiedResult => {
+  return {
+    name: r.name,
+    params: r.params,
+    next: r.next != null ? simplifyResult(r.next) : null
+  };
+};
+
+let rules: IRouteRule[] = [
+  {
+    path: "a",
+    next: [{ path: "b" }, { path: "c/:x", next: [{ path: "d/:y" }] }, { path: ":z" }]
+  }
+];
+
+test("test parsing basic rule", () => {
+  let actual = simplifyResult(parseRoutePath("/a", rules));
+  let goal: ISimplifiedResult = { name: "a", next: null, params: {} };
+  expect(actual).toEqual(goal);
+});
+
+test("test parsing nested path", () => {
+  let actual = simplifyResult(parseRoutePath("/a/b", rules));
+  let goal: ISimplifiedResult = { name: "a", next: { name: "b", next: null, params: {} }, params: {} };
+  expect(actual).toEqual(goal);
+});
+
+test("test parsing path with variables", () => {
+  let actual = simplifyResult(parseRoutePath("/a/c/10", rules));
+  let goal: ISimplifiedResult = { name: "a", next: { name: "c", next: null, params: { x: "10" } }, params: {} };
+  expect(actual).toEqual(goal);
+});
+
+test("test parsing path with multiple variables", () => {
+  let actual = simplifyResult(parseRoutePath("/a/c/10/d/22", rules));
+  let goal: ISimplifiedResult = {
+    name: "a",
+    next: { name: "c", next: { name: "d", next: null, params: { x: "10", y: "22" } }, params: { x: "10" } },
+    params: {}
+  };
+  expect(actual).toEqual(goal);
+});
+
+test("test parsing dynamic path", () => {
+  let actual = simplifyResult(parseRoutePath("/a/33", rules));
+  let goal: ISimplifiedResult = { name: "a", next: { name: ":z", next: null, params: { z: "33" } }, params: {} };
+  expect(actual).toEqual(goal);
+});
+
+test("test parsing path with no rules", () => {
+  let actual = simplifyResult(parseRoutePath("/m", rules));
+  let goal: ISimplifiedResult = { name: "404", next: null, params: {} };
+  expect(actual).toEqual(goal);
+});

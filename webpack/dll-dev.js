32eca4cfff8fd59cf01f089d2703c5367ed69fc1
jiyinyiyong <jiyinyiyong@gmail.com>
2020-08-27 14:46:52 +0800

    drop immer dependency

--- webpack/dll-dev.js
+++ webpack/dll-dev.js
@@ -6,22 +6,22 @@ let { matchCssRule, matchFontsRule } = require("./shared");
 
 module.exports = {
   mode: "development",
-  entry: ["react", "react-dom", "emotion", "immer", "dayjs"],
+  entry: ["react", "react-dom", "emotion", "dayjs"],
   output: {
     filename: "dll_vendors_[hash:8].js",
     path: path.join(__dirname, "dll"),
-    library: "dll_vendors_[hash:8]"
+    library: "dll_vendors_[hash:8]",
   },
   devtool: "cheap-source-map",
   module: {
-    rules: [matchCssRule, matchFontsRule]
+    rules: [matchCssRule, matchFontsRule],
   },
   plugins: [
     new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /zh-cn/),
     new webpack.NamedModulesPlugin(),
     new webpack.DllPlugin({
       name: "dll_vendors_[hash:8]",
-      path: path.join(__dirname, "dll/manifest.json")
-    })
-  ]
+      path: path.join(__dirname, "dll/manifest.json"),
+    }),
+  ],
 };
